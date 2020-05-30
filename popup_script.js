0aca074d854177d06338b13ee1779219dd842167
Jordan Ferguson <65615665+jordannfergusonn@users.noreply.github.com>
2020-05-30 12:39:41 -0500

    attempt

---
+++ popup_script.js
@@ -0,0 +1,7 @@
+function link(website) {
+  window.open(website);
+}
+
+document.getElementById('button1').addEventListener('click', link("https://registrar.utexas.edu/schedules"));
+document.getElementById('button2').addEventListener('click', link("https://onestop.utexas.edu/registration-and-degree-planning/registering-for-classes/registration-times"));
+document.getElementById('button3').addEventListener('click', link("https://utdirect.utexas.edu/apps/registrar/ut_planner"));
