3a7d634e6c534a6a6c487dc38983171fc009b817
Jordan Ferguson <65615665+jordannfergusonn@users.noreply.github.com>
2020-05-30 13:19:19 -0500

    attempt

--- popup_script.js
+++ popup_script.js
@@ -1,16 +1,3 @@
-function link(website) {
-  window.open(website);
-}
-
-var but1 = document.getElementById('button1');
-if (but1) {
-    but1.addEventListener('click', function() {link("https://registrar.utexas.edu/schedules")}, false);
-}
-var but2 = document.getElementById('button2');
-if (but2) {
-    but2.addEventListener('click', function() {link("https://onestop.utexas.edu/registration-and-degree-planning/registering-for-classes/registration-times")}, false);
-}
-var but3 = document.getElementById('button3');
-if (but3) {
-    but3.addEventListener('button3').addEventListener('click', function() {link("https://utdirect.utexas.edu/apps/registrar/ut_planner")}, false);
+function Open() {
+  window.open("https://registrar.utexas.edu/schedules","_blank");
 }
