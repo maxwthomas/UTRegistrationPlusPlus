7f3a509c4d9aa0a714d9a53312871cb8aea99c25
Jordan Ferguson <65615665+jordannfergusonn@users.noreply.github.com>
2020-06-02 18:50:04 -0500

    test

--- popup_script.js
+++ popup_script.js
@@ -1,6 +1,11 @@
-document.getElementById("myButton").addEventListener("click", myFunction);
-
-function myFunction(){
+// Changes popup to dark mode
+function darkMode(){
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+$(document).ready(function() {
+  $("#moon").click(function() {
+      darkMode();
+  });
+});
