7186611b3995676bb2d978ffc9b8f2ae73f8adc1
Jordan Ferguson <65615665+jordannfergusonn@users.noreply.github.com>
2020-06-04 16:39:06 -0500

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,7 @@
 //if (document.title.indexOf("Google") != -1) {
     //Creating Elements
     var btn = document.createElement("BUTTON")
-    var t = document.createTextNode("CLICK ME!");
+    var t = document.createTextNode("CLICK ME");
     btn.appendChild(t);
     //Appending to DOM 
     document.body.appendChild(btn);
