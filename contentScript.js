4e3607cd4be69307667f659994088692d3a1beaf
Jordan Ferguson <65615665+jordannfergusonn@users.noreply.github.com>
2020-06-05 18:47:22 -0500

    buttons

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,21 @@
+// Header
+var header = document.createElement("th");
+var txt = document.createTextNode("Plus Plus");
+header.appendChild(txt);
+document.getElementsByClassName("rwd-table results")[0].firstChild.nextSibling.firstChild.nextSibling.appendChild(header);
 
-//Creating Elements
-var btn = document.createElement("th");
-var t = document.createTextNode("Plus Plus");
-btn.appendChild(t);
+// Icon
+var im;
+var but;
+var i;
+var info = document.getElementsByClassName("rwd-table results")[0].firstChild.nextSibling.nextSibling.nextSibling;
+var cur;
+for (i = 0; i < info.children.length; i++) {
+    im = document.createElement("BUTTON");
+    im.innerHTML = '<img src="images/longhorn.png" />';
 
-    
-document.getElementsByClassName("rwd-table results")[0].firstChild.nextSibling.firstChild.nextSibling.appendChild(btn);
+    cur = info.children[i];
+    if (cur.firstChild.nextSibling.className != "course_header") {
+        cur.appendChild(im);
+    }
+}
