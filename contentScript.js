df89735d7d9151ef77cfe4d9595bd41c7f49fa9b
Jordan Ferguson <65615665+jordannfergusonn@users.noreply.github.com>
2020-06-07 01:52:24 -0500

    button

--- contentScript.js
+++ contentScript.js
@@ -12,9 +12,15 @@ var info = document.getElementsByClassName("rwd-table results")[0].firstChild.ne
 var cur;
 for (i = 0; i < info.children.length; i++) {
     im = document.createElement("BUTTON");
-    im.innerHTML = '<img src="images/longhorn.png" />';
+    im.innerHTML = '<img src="https://i.imgur.com/bThluov.png" />';
+    im.style.cssText = "padding: 10px; display: block; max-width: 80px; margin: auto; cursor: pointer; border: none; background-color: rgb(244,244,244)";
 
     cur = info.children[i];
+    // Closed
+    if (cur.className == "unavailable") {
+        im.style.cssText = "padding: 10px; display: block; max-width: 80px; margin: auto; cursor: pointer; border: none; background-color: rgb(204,204,204)";
+    }
+
     if (cur.firstChild.nextSibling.className != "course_header") {
         cur.appendChild(im);
     }
