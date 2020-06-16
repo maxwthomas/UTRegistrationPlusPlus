2a7d55c15b868b51c3edc2df4a34530c3d90a1f1
Jordan Ferguson <65615665+jordannfergusonn@users.noreply.github.com>
2020-06-15 21:27:00 -0500

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -4,6 +4,8 @@ script.src = '/node_modules/jquery/dist/jquery.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
+// Import bootstrap
+
 // Header
 var header = document.createElement("th");
 header.scope = "col";
@@ -20,11 +22,13 @@ var cur;
 for (i = 0; i < info.children.length; i++) {
 
     // Attach image to longhorn button
-    longhorn = document.createElement("td");
+    longhorn = document.createElement("button");
+    longhorn.type = "button";
+    longhorn.class = "btn btn-primary";
+    longhorn.id = "ourButton";
     longhorn.style.cssText = "color: rgb(51, 51, 51); text-decoration: none; font-weight: normal";
     pic = document.createElement("input");
     pic.type = "image";
-    pic.id = "ourButton";
     pic.width = "40";
     pic.height = "20";
     pic.src = "https://i.imgur.com/bThluov.png";
@@ -38,8 +42,9 @@ for (i = 0; i < info.children.length; i++) {
 }
 
 // Event listener for click
-document.getElementById("ourButton").addEventListener("click", getOptions());
-
-// Function for options popup
-function getOptions() { 
-}
+//document.getElementById("ourButton").addEventListener("click", getOptions());
+$(document).ready(function() {
+  $("#ourButton").click(function() {
+    //$("#ourModal").modal();
+  });
+});
