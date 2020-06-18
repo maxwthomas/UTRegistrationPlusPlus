ea83b4169507f462a2dd66e286d2ecd524aded99
Jordan Ferguson <65615665+jordannfergusonn@users.noreply.github.com>
2020-06-17 20:12:49 -0500

    imports

--- popup_script.js
+++ popup_script.js
@@ -1,8 +1,8 @@
 // Import jquery
-var script = document.createElement('script');
-script.src = '/node_modules/jquery/dist/jquery.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+// var script = document.createElement('script');
+// script.src = 'jquery.js';
+// script.type = 'text/javascript';
+// document.getElementsByTagName('head')[0].appendChild(script);
 
 // Changes popup to dark mode
 function darkMode(){
