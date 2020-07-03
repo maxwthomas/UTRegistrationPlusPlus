8f6a68946fe81836126edc7fb019383d8cc70f3b
Jordan Ferguson <65615665+jordannfergusonn@users.noreply.github.com>
2020-07-02 20:42:41 -0500

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -26,32 +26,29 @@ for (i = 0; i < info.children.length; i++) {
 }
 
 // Modal
-var modal_content = '<div class="modal fade" id="ourModal">' +
+var modal_content = '<div class="modal fade" id="ourModal" role="dialog" aria-hidden="false">' +
+'<div class="modal-dialog">' + 
+
 '<div class="modal-content">' +
-  '<div class="modal-header" style="padding:35px 50px;">' +
+
+  '<div class="modal-header">' +
+    '<h4 class="modal-title">Course Name</h4>' +
     '<button type="button" class="close" data-dismiss="modal">&times;</button>' +
-    '<h4><span class="glyphicon glyphicon-lock"></span> Login</h4>' +
   '</div>' +
-  '<div class="modal-body" style="padding:40px 50px;">' +
-    '<form role="form">' +
-      '<div class="form-group">' +
-        '<label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>' +
-        '<input type="text" class="form-control" id="usrname" placeholder="Enter email">' +
-      '</div>' +
-      '<div class="form-group">' +
-        '<label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>' +
-        '<input type="text" class="form-control" id="psw" placeholder="Enter password">' +
-      '</div>' +
-      '<div class="checkbox">' +
-        '<label><input type="checkbox" value="" checked>Remember me</label>' +
-      '</div>' +
-        '<button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Login</button>' +
-    '</form>' +
+
+  '<div class="modal-body">' +
+    '<p style="box-shadow: 5px 5px 20px rgb(161, 161, 161)">' +
+    '<button class="btn btn-primary" id="rmp" type="button">Rate My Prof</button>' +
+    '<button class="btn btn-primary" id="ecis" type="button">eCIS</button>' +
+    '<button class="btn btn-primary" id="catalyst" type="button">UT Catalyst</button>' +
+    '<button class="btn btn-primary" id="prereq" type="button">Prereqs</button>' +
+    '<button class="btn btn-primary" id="textbook" type="button">Textbooks</button>' +
+    '<button class="btn btn-primary" id="syllabi" type="button">Syllabi</button>' +
+    '</p>' +
   '</div>' +
+
   '<div class="modal-footer">' +
-    '<button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>' +
-    '<p>Not a member? <a href="#">Sign Up</a></p>' +
-    '<p>Forgot <a href="#">Password?</a></p>' +
+    '<button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Add Class</button>' +
   '</div></div></div></div>';
 document.querySelector("#container").innerHTML = modal_content + document.querySelector("#container").innerHTML;
 
