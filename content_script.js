f8e9e14e378d04859faa8c88aa972350252bdaf7
Jordan Ferguson <65615665+jordannfergusonn@users.noreply.github.com>
2020-07-29 19:27:15 -0500

    test

--- content_script.js
+++ content_script.js
@@ -47,7 +47,7 @@ var modal_content = "<div class='modal fade' id='plusPlusModal' role='dialog'>" 
       "</div>" +
 
       "<div class='modal-footer'>" +
-        "<button type='submit' class='btn btn-success btn-block'>Add Class</button>" +
+        "<button type='submit' class='btn btn-success btn-block' id='plusPlusAddClass'>Add Class</button>" +
         "<button class='plusPlusDarkBtn' id='plusPlusMoon'><i class='fa fa-moon-o' id='plusPlusDarkSymbol'></i></button>" +
       "</div>" +
 
@@ -75,7 +75,11 @@ $("#plusPlusMoon").click(function() {
 
 var course_name;
 var prof_name;
+var unique_number_element;
 var unique_number;
+var days;
+var times;
+var building;
 var more_info_link;
 $(".plusPlusLonghornBtn").click(function() {
   // Get course name
@@ -98,6 +102,58 @@ $(".plusPlusLonghornBtn").click(function() {
   }
 
   // Get link to more information
-  unique_number = $(this).parent().siblings().filter("[data-th='Unique']")[0];
-  more_info_link = $(unique_number).children()[0].href;
+  unique_number_element = $(this).parent().siblings().filter("[data-th='Unique']")[0];
+  unique_number = unique_number_element.textContent;
+  more_info_link = $(unique_number_element).children()[0].href;
+
+  // Get days, times, and building
+  days = $(this).parent().siblings().filter("[data-th='Days']")[0].textContent;
+  times = $(this).parent().siblings().filter("[data-th='Hour']")[0].textContent;
+  building = $(this).parent().siblings().filter("[data-th='Room']")[0].textContent;
 });
+
+// Add class to student schedule
+$("#plusPlusAddClass").click(function() {
+  var course = new Course(course_name, prof_name, days, times, building, unique_number);
+  var days_list = days.trim().split("  ");
+  var times_list = times.trim().split("  ");
+  
+  // TODO: fix thursday, calendar start/end days (past 30/31)
+  const FIRST_MONDAY = 3;
+  const LAST_MONDAY = 28;
+  const WEEKDAYS = {"M":0, "T":1, "W":2, "TH":3, "F":4};
+  var time_details = [];
+  for (var i = 0; i < days_list.length; i++) {
+    var days_group = days_list[i];
+
+    // determine time to start and end
+    var hours = times_list[i].split("-");
+    var start_hour = hours[0];
+    var end_hour = hours[1];
+    
+    alert(hours);  
+
+    // determine length of course in seconds
+
+    for (var day = 0; day < days_group.length; day++) {
+      var day_letter = days_group[day];
+
+      // determine day to start and end
+      var start_day = FIRST_MONDAY + WEEKDAYS[day_letter];
+      if (start_day < 10) {
+        start_day = "0" + start_day;
+      } else {
+        start_day = "" + start_day;
+      }
+      var end_day = LAST_MONDAY + WEEKDAYS[day_letter];
+      if (end_day < 10) {
+        end_day = "0" + end_day;
+      } else {
+        end_day = "" + end_day;
+      }
+      var day_time_details = ["2020-08-" + start_day, "2020-12-" + end_day, 10]
+      time_details.push(day_time_details);
+    }
+  }
+  alert(time_details);
+})
--- schedule.js
+++ schedule.js
@@ -1,9 +1,79 @@
+// class to represent a schedule for a student
+class Schedule {
+
+    constructor(semester) {
+        this.semester = semester;
+        this.courses = [];
+    }
+
+    // adds a course to the schedule by creating a course object and storing it
+    // given course name, course time, building, unique number, and more info link
+    addCourse(name, days, times, building, unique, link) {
+        const query = new Query(link);
+        const professor = query.fullName();
+        const course = new Course(name, professor, days, times, building, unique);
+        this.courses.push(course);
+        Storage.storeOne(unique, course);
+    }
+
+    // removes a course from the schedule
+    removeCourse(unique) {
+        // find course
+        var swapped = false;
+        var i = 0;
+        while (!swapped && i < this.courses.length) {
+            if (this.courses[i] == course) {
+                // swap course object to end
+                swapped = true;
+                this.courses[i] = this.courses[this.courses.length - 1];
+                this.courses[this.courses.length - 1] = course;
+            }
+        }
+        if (swapped) {
+            this.courses.pop();
+        }
+        Storage.clearOne(unique);
+    }
+
+    // removes all courses from the schedule
+    removeAllCourses() {
+        this.courses = [];
+        Storage.clearAll();
+    }
+
+    // displays the schedule by getting information from the storage and updating the scheduler
+    static displaySchedule(schedule) {
+        // TODO: determine the starting date to display
+        scheduler.init('scheduler_here', new Date(), "week");
+
+        // get courses from storage class (dictionary {unique: course object})
+        const courses = Storage.retrieveAll(schedule);
+        
+        // create a list of dictionaries of the specifics for each course
+        const courses_json_format = [];
+        Object.keys(courses).forEach(function(key) {
+            var course_object = courses[key];
+            var course_time = {id:key, text:course_object.courseName(), start_date:"", end_date:"", rec_type:"week_1___", event_length:course_object.length(), event_pid:"0"};
+            courses_json_format.push(course_time);
+        });
+        scheduler.parse(courses_json_format, "json");
+    }
+}
+
 scheduler.init('scheduler_here', new Date(), "month");
 
 var events = [
     {id:1, text:"Meeting",   start_date:"07/11/2020 14:00",end_date:"07/11/2020 17:00"},
     {id:2, text:"Conference",start_date:"07/15/2020 12:00",end_date:"07/18/2020 19:00"},
-    {id:3, text:"Interview", start_date:"07/24/2020 09:00",end_date:"07/24/2020 10:00"}
- ];
-  
- scheduler.parse(events, "json");
+    {id:3, text:"Interview", start_date:"07/24/2020 09:00",end_date:"07/24/2020 10:00"},
+    {id:4, 
+      start_date:"2020-07-03 10:00:00",
+     end_date:"2020-07-13 00:00:00",
+     text:"some_text",
+     details:"",
+     rec_type:"week_1___",
+     event_length:"7200",
+     event_pid:"0"}
+];
+
+scheduler.parse(events, "json");
